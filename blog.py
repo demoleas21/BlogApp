234aa95cfe71c0438d336b6882612ec16f6145d2
demoleas21 <andrew.demoleas@gmail.com>
2016-05-27 23:14:01 -0400

    Basic controller

---
+++ blog.py
@@ -0,0 +1,19 @@
+"""Controller"""
+
+import sqlite3
+from flask import Flask, render_template, request, session, flash, redirect, url_for, g
+
+# Configuration
+DATABASE = 'blog.py'
+
+app = Flask(__name__)
+
+# Pulls in app configuration by looking for UPPERCASE variables
+app.config.from_object(__name__)
+
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+if __name__ == '__main__':
+    app.run(debug=True)
