0ecab75f5c42749809dc82676222dac0a7f0c3fe
demoleas21 <andrew.demoleas@gmail.com>
2016-05-27 23:49:49 -0400

    View files

--- blog.py
+++ blog.py
@@ -15,5 +15,15 @@ app.config.from_object(__name__)
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
+
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/main')
+def main():
+    return render_template('main.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
